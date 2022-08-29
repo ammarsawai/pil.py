48721c0a29ad7860d377ced271004187c43667e0
ammarsawai <30580614+ammarsawai@users.noreply.github.com>
2022-08-29 10:46:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,8 @@ rotated_image = image_obj.rotate(90)
 rotated_image.save("messi_rotated.jpg")
 rotated_image.image
 
+Error:
+Exception has occurred: AttributeError
+'NoneType' object has no attribute 'rotate'
+  File "/Users/ammaralsawai/Desktop/pil.py/main.py", line 3, in <module>
+    rotated_image = image_obj.rotate(90)
