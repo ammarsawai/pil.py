38f442a67a29fff9b1ff3d476f04cb3ab4006412
ammarsawai <sawaiammar@gmail.com>
2022-08-29 10:43:34 -0400

    help

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from PIL import Image
-image_obj = Image.open("messi.jpg").convert('RGB').save('new.jpeg')
+image_obj = Image.open("messi.jpg").convert('RGB').save('messi.jpeg')
 rotated_image = image_obj.rotate(90)
 rotated_image.save("messi_rotated.jpg")
 rotated_image.image
